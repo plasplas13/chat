ac244206d79a3811be56821e59bc3a43554a6319
plasplas13 <113336933+plasplas13@users.noreply.github.com>
2024-04-07 17:08:10 +0000

    adding files

---
+++ big-agi/src/common/providers/ProviderBootstrapLogic.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useRouter } from 'next/router';
+
+import { markNewsAsSeen, shallRedirectToNews } from '../../apps/news/news.version';
+
+import { navigateToNews, ROUTE_APP_CHAT } from '~/common/app.routes';
+import { useNextLoadProgress } from '~/common/components/useNextLoadProgress';
+
+
+export function ProviderBootstrapLogic(props: { children: React.ReactNode }) {
+
+  // external state
+  const { route, events } = useRouter();
+
+  // wire-up the NextJS router to a loading bar to be displayed while routes change
+  useNextLoadProgress(route, events);
+
+
+  // logic
+  const doRedirectToNews = (route === ROUTE_APP_CHAT) && shallRedirectToNews();
+
+
+  // redirect Chat -> News if fresh news
+  const isRedirecting = React.useMemo(() => {
+    if (doRedirectToNews) {
+      // the async is important (esp. on strict mode second pass)
+      navigateToNews().then(() => markNewsAsSeen());
+      return true;
+    }
+    return false;
+  }, [doRedirectToNews]);
+
+
+  return isRedirecting ? null : props.children;
+}
